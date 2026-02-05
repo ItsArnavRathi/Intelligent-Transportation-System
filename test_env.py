96460975bae94c780d90fa29447d0657522d68f0
Arnav Rathi <arnav.rathi2@gmail.com>
2026-02-05 11:28:48 +0530

    Environment setup and dependency installation

---
+++ test_env.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+print("NumPy OK:", np.__version__)
+print("Pandas OK:", pd.__version__)
+print("TensorFlow OK:", tf.__version__)
